69db8d7657b0d4ad830e2cb7bd568d17e5d21e14
Hoàng Minh Tâm <tam.hm.61cntt@ntu.edu.vn>
2025-06-08 14:18:12 +0700

    Comit Update CORS Middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const allowedOrigins = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}));
 
 app.use('/api/auth', authRoutes);
 
